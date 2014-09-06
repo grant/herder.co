924b88afbfda4d729d4878c94c4eb1bd072cfde7
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 16:30:54 -0700

    init appjs

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+// Other
+app.configure(function(){
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
