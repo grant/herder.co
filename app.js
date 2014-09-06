7f69a70b04fa40fe10a6c296b75ddf02eca0197a
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 16:33:28 -0700

    Clean up app js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  * Module dependencies.
  */
 
+var http = require('http');
 var express = require('express');
 var app = express();
 
@@ -10,16 +11,10 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-// Other
-app.configure(function(){
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('*', function (req, res) {
+  res.send('hi');
 });
