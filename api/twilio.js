607de5876cfe336ab8c87e452514d2b052a11fbe
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 23:05:53 -0700

    Add twilio support

--- api/twilio.js
+++ api/twilio.js
@@ -1,14 +1,14 @@
 var accountSid = 'AC8b1c9bf970c71033309d1ee72b9cc982';
-var authToken = '';
+var authToken = process.env.TWILIO_AUTH_TOKEN;
 var client = require('twilio')(accountSid, authToken);
  
-client.messages.create({
-    body: "Jenny please?! I love you <3",
-    to: "+14159352345",
-    from: "+14158141829"
-}, function(err, message) {
-    process.stdout.write(message.sid);
-});
+// client.messages.create({
+//     body: "Jenny please?! I love you <3",
+//     to: "+14159352345",
+//     from: "+14158141829"
+// }, function(err, message) {
+//     process.stdout.write(message.sid);
+// });
 
 // Example options:
 // {
@@ -17,5 +17,7 @@ client.messages.create({
 //   from: "+14158141829"
 // }
 module.exports = function (options) {
-
+  client.messages.create(options, function (err, message) {
+    console.log('sent message');
+  });
 };
