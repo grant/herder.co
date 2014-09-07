1a12dd2563e496128821f3cf9dce5dc5081b1eb5
Grant Timmerman <granttimmerman@gmail.com>
2014-09-07 12:43:26 -0700

    Fix css coloring

--- api/twilio.js
+++ api/twilio.js
@@ -1,5 +1,5 @@
 var accountSid = 'AC8b1c9bf970c71033309d1ee72b9cc982';
-var authToken = 'a6064e37d93a75a209bd944de3543e7e';
+var authToken = process.env.TWILIO_AUTH_TOKEN;
 var client = require('twilio')(accountSid, authToken);
 
 var sendMessage = function (driverContact, surge, wait, expectedTrend, callback) {
