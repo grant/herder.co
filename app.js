d648eeed663ecfb1f2c20860f99188dda52c9baf
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 20:00:12 -0700

    Added css

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var http = require('http');
 var jade = require('jade');
 
 var express = require('express');
-var uber = require('./api/uber');
+// var uber = require('./api/uber');
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(express.static('public'));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
