25d25e1ccd8841d106d906f92e715f0e991c8c36
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 17:29:00 -0700

    Update appjs

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-// app.set('views', rootDir + 'views');
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
