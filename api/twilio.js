d510a7187ff7d8ea8f213ff9c5d60d5e4cec8f29
michaelfarinacci <michael@viptela.com>
2014-09-07 08:51:12 -0700

    add twilio sample call

--- api/twilio.js
+++ api/twilio.js
@@ -1,23 +1,18 @@
 var accountSid = 'AC8b1c9bf970c71033309d1ee72b9cc982';
 var authToken = process.env.TWILIO_AUTH_TOKEN;
 var client = require('twilio')(accountSid, authToken);
- 
-// client.messages.create({
-//     body: "Jenny please?! I love you <3",
-//     to: "+14159352345",
-//     from: "+14158141829"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
 
-// Example options:
-// {
-//   body: "Jenny please?! I love you <3",
-//   to: "+14159352345",
-//   from: "+14158141829"
-// }
-module.exports = function (options) {
-  client.messages.create(options, function (err, message) {
-    console.log('sent message');
+var sendMessage = function (driverContact, surge, wait, expectedTrend, callback) {
+  var smsBody = "surge: " + surge + '\n' + "wait: " + wait + '\n'
+  smsBody = smsBody + expectedTrend
+  client.messages.create({
+    body: smsBody,
+    to: driverContact,
+    from: driverContact
+  }, function(err, message) {
+    process.stdout.write(message.sid);
   });
-};
+
+}
+
+module.exports.sendMessage = sendMessage;
