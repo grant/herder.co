c7aa2da027d6123c2a8c4440a3a641563081c6cf
Grant Timmerman <granttimmerman@gmail.com>
2014-09-07 06:02:07 -0700

    enable create heatmap overlay

--- public/js/index.js
+++ public/js/index.js
@@ -52,23 +52,23 @@ $(function () {
   // standard map
   map = new google.maps.Map(document.getElementById("map"), myOptions);
   // heatmap layer
-  // heatmap = new HeatmapOverlay(map, {
-  //   // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-  //   "radius": 0.2,
-  //   "maxOpacity": 1,
-  //   // scales the radius based on map zoom
-  //   "scaleRadius": true,
-  //   // if set to false the heatmap uses the global maximum for colorization
-  //   // if activated: uses the data maximum within the current map boundaries 
-  //   //   (there will always be a red spot with useLocalExtremas true)
-  //   "useLocalExtrema": true,
-  //   // which field name in your data represents the latitude - default "lat"
-  //   latField: 'lat',
-  //   // which field name in your data represents the longitude - default "lng"
-  //   lngField: 'lng',
-  //   // which field name in your data represents the data value - default "value"
-  //   valueField: 'count'
-  // });
+  heatmap = new HeatmapOverlay(map, {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    "radius": 0.2,
+    "maxOpacity": 1,
+    // scales the radius based on map zoom
+    "scaleRadius": true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries 
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": true,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  });
 
   // // Try HTML5 geolocation
   // if (navigator.geolocation) {
