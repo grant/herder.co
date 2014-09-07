7738193844beb6b673517a780f014a619109b0f4
Grant Timmerman <granttimmerman@gmail.com>
2014-09-07 05:56:48 -0700

    enable part of gmaps

--- public/js/index.js
+++ public/js/index.js
@@ -42,33 +42,33 @@ $(function () {
   // var waitChart = new Chart(waitChartCtx);
 
   // map center
-  // var myLatlng = new google.maps.LatLng(37.332017799999996, -121.8899544);
-  // // map options,
-  // var myOptions = {
-  //   zoom: 10,
-  //   center: myLatlng,
-  //   disableDefaultUI: true
-  // };
-  // // standard map
-  // map = new google.maps.Map(document.getElementById("map"), myOptions);
-  // // heatmap layer
-  // heatmap = new HeatmapOverlay(map, {
-  //   // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-  //   "radius": 0.2,
-  //   "maxOpacity": 1,
-  //   // scales the radius based on map zoom
-  //   "scaleRadius": true,
-  //   // if set to false the heatmap uses the global maximum for colorization
-  //   // if activated: uses the data maximum within the current map boundaries 
-  //   //   (there will always be a red spot with useLocalExtremas true)
-  //   "useLocalExtrema": true,
-  //   // which field name in your data represents the latitude - default "lat"
-  //   latField: 'lat',
-  //   // which field name in your data represents the longitude - default "lng"
-  //   lngField: 'lng',
-  //   // which field name in your data represents the data value - default "value"
-  //   valueField: 'count'
-  // });
+  var myLatlng = new google.maps.LatLng(37.332017799999996, -121.8899544);
+  // map options,
+  var myOptions = {
+    zoom: 10,
+    center: myLatlng,
+    disableDefaultUI: true
+  };
+  // standard map
+  map = new google.maps.Map(document.getElementById("map"), myOptions);
+  // heatmap layer
+  heatmap = new HeatmapOverlay(map, {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    "radius": 0.2,
+    "maxOpacity": 1,
+    // scales the radius based on map zoom
+    "scaleRadius": true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries 
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": true,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  });
 
   // // Try HTML5 geolocation
   // if (navigator.geolocation) {
