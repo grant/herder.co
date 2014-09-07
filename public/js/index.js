71b37fccb56fb18d58ab2f3d485f2b713cc3ca1e
Grant Timmerman <granttimmerman@gmail.com>
2014-09-07 02:50:53 -0700

    Add basic styling and graph

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,36 @@
 $(function () {
-  
+  var $priceChart = $('#price');
+  var priceChartCtx = $priceChart.get(0).getContext('2d');
+  var $waitChart = $('#wait');
+  var waitChartCtx = $waitChart.get(0).getContext('2d');
+
+  priceChartCtx.canvas.width = $priceChart.width();
+  waitChartCtx.canvas.width = $waitChart.width();
+  var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  };
+  var priceChart = new Chart(priceChartCtx).Line(data);
+  // var waitChart = new Chart(waitChartCtx);
 });
