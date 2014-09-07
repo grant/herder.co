92db8d8d0d823f5320c171c0e37afdbb484afa42
Grant Timmerman <granttimmerman@gmail.com>
2014-09-06 18:57:33 -0700

    Added uber api data

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.render('main');
 });
 
